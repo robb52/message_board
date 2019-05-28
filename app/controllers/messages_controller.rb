96e57cb8250abf0c2b1be5320bb2f4c29fcebf66
Rob Brown <brown4homes@yahoo.com>
2019-05-28 14:12:35 -0700

    Generated message model and contoller, added index, new, create actions installed simple form and bootstrap

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+
+  def index
+
+  end
+
+  def new 
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :description)
+  end
+
+end
