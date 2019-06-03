a307de8d00058e000430f8ea1c5eabfa593e24f5
Rob Brown <brown4homes@yahoo.com>
2019-06-03 10:16:37 -0700

    Added user authentication to message and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   before_action :find_message
   before_action :find_comment, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+
 
   def create
     @comment = @message.comments.create(comment_params)
@@ -22,6 +24,11 @@ class CommentsController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to message_path(@message)
   end
 
   private
